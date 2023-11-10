93b50074b7ecfccb3245a3c831d5040a28a99e6f
krrattoss5 <krrattoss5@gmail.com>
2023-11-09 19:41:51 -0500

    controllers, gitignore

--- dist/app.js
+++ dist/app.js
@@ -6,13 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.App = void 0;
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
+const index_routes_1 = __importDefault(require("./routes/index.routes"));
 class App {
     constructor() {
         this.app = (0, express_1.default)();
         this.middlewares();
+        this.routes();
     }
     middlewares() {
         this.app.use((0, morgan_1.default)('dev'));
+    }
+    routes() {
+        this.app.use(index_routes_1.default);
     }
     listen() {
         this.app.listen(3000, () => console.log('listening on http://localhost:3000'));
